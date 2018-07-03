588e45b8f8ef37dbebd4187a32b1ffa32230d374
Afa Madza <305@holbertonschool.com>
2018-07-03 18:45:49 +0000

    Added updated header file and function to find tree size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - finds the size of a binary tree
+ * @tree: pointer to root node
+ * Return: size of tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left;
+	size_t right;
+	size_t size = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+	size += (left + right + 1);
+	return (size);
+}
